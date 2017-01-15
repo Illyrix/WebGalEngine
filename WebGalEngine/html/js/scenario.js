26790483ca57628c66706af7a1e2660cb1038499
Illyrix <lwhcvictor@outlook.com>
2017-01-15 16:10:38 +0800

    rewrite Proc and Scenario, add wait to Control

--- WebGalEngine/html/js/scenario.js
+++ WebGalEngine/html/js/scenario.js
@@ -1,27 +1,36 @@
-window.Scenario = function *(){
-    yield function(){console.log("rf1");};
-    
-    yield function(){console.log("rf2");};
+(function(){
+    var temp_var = true;
+    Engine.Scenario = Engine.Scenario.concat(
+        new Array(
+            function(){console.log("step0"); Engine.Control.wait(1500, function(){console.log(Engine.Control);Engine.Control.wait(1500,function(){console.log(Engine.Control);});});},
+            function(){Engine.Proc.goto("CH1");console.log("step1");},
+            {label:"CH0"},  // 设置标签CH0
+            function(){
+                temp_var = false;
+                console.log("step2");
+                Engine.Proc.goto("CH2");
+            },
+            {label:"CH1"},
+            function(){console.log("step3")},
+            function(){
+                temp_var = true;
+                console.log("step4");
+            },
+            {label:"CH2"},
+            function(){
+                console.log("step5");
+                if (temp_var) Engine.Proc.goto("CH0");
+            },
+            function(){console.log("step6");}
+        )
+    );
+}())
 
-    const SWITCH_OTHER_BRANCH = true;
-    if (SWITCH_OTHER_BRANCH) 
-        yield* branch1();
-    else
-        yield* branch2();
-    
-    yield function(){console.log("rf3");};
-    
-    yield function(){console.log("rf4");};
-    
-    yield function(){console.log("rf5");};
-}
-
-function *branch1() {
-    yield function(){console.log("角色线A");};
-    yield function(){console.log("do something");};
-}
-
-function *branch2() {
-    yield function(){console.log("角色线B");};
-    yield function(){console.log("do other things");};
-}
+/* 
+在其他文件引入的时候也是这种格式:
+Engine.Scenario = Engine.Scenario.concat(
+    new Array(
+    //..其他文件中的场景流程..
+    )
+);
+*/
--- WebGalEngine/html/js/webgalengine.js
+++ WebGalEngine/html/js/webgalengine.js
@@ -1,67 +1,84 @@
-window.Engine = new function(){
+Engine = new function(){
+
+    this.Scenario = new Array();
 
     // 对与剧情分支有关的全局/局部变量的存储
-    /*
-     * 使用window.Engine.Vars.varName之前需要先将varName设置setter:
-     * Usage: window.Engine.Vars.addVar("varName"); window.Engine.Vars.varName = 1;
-     */
     this.Vars = new function() {
-        this._vars = {};
-        this.addVar = function (str){
-            Object.defineProperty(this, str, {
-                get:function(){
-                    // Todo
-                    console.log("vars getter: "+this._vars);
-                    return this._vars[str];
-                },
-                set:function(v){
-                    // Todo:将对var的更改的新旧值均写入历史数组进行存储
-                    console.log("vars setter: "+this._vars+" to "+v);
-                    this._vars[str] = v;
-                }
-            }); 
-        }
+        // 在任何分支/周目都通用的变量(一般是某些设置等)
+        this.Global = {};
+        // 会随着save操作保存进档案的对剧情有影响的变量
+        this.Local = {};
+        // 每次load操作/重启之后会清空
+        this.Temp = {};
     }
 
     // 对程序流程(剧情)进行控制
-    // func 是编写有剧情的生成器函数, 默认为 window.Scenario
-    // 存放在./scenario.js里
-    this.Proc = new function(func) {
-        var processGen = function *gen(generator){
-            // 步进到某个场景需要的步数
-            // Important: 在进行Save/Load之后同一场景对应的i不一定相同
-            // 只有在步进的时候将环境变量值还原才一定相同
-            let i = 0;
-            let now;
-            let realfunc = generator();
-            var exec = {done:false};
-
-            while(! exec.done) {
-                now = yield i;
-                exec = realfunc.next();
-                if ((!now || now == i) && !exec.done) {
-                    exec.value();
+    // 一般存放在./scenario.js里
+    this.Proc = new function() {
+        // 对流程的初始化工作, 比如确定每个label等
+        // 需要在Engine对象创建完成之后调用一遍
+        this.init = function(){
+            var length = Engine.Scenario.length;
+            for (let i = 0; i < length; i++) {
+                let t = Engine.Scenario[i];
+                if (typeof(t) == "object" && t.label != undefined) {
+                    Engine.Proc.scenarios[i] = {label:t.label, index:i};     // func什么都不做
+                    if (Engine.Proc.labels[t.label] == undefined)       // 避免重复的label
+                        Engine.Proc.labels[t.label] = i;
+                    else
+                        throw new SyntaxError("Label is duplicated");
+                }else {
+                    Engine.Proc.scenarios[i] = {index:i, func:t};
                 }
-                i++;
-            }
-        }
-
-        // 存储当前场景
-        this.ptr = processGen(func);
-
-        // 调用next()即进入下一个场景
-        this.next = function(){this.ptr.next()};
-
-        // 快进到某一到过的场景(即Load)
-        this.goto = function(position){
-            var t = this.ptr.next(position);
-            while (t.value != position && t.done == false) {
-                //todo: 添加对Vars的更改
-                t = this.ptr.next(position);
             }
         };
-    }(window.Scenario);
+        // 存储分析后的Scenario:{[label:"xx",] index:5, func:callable}
+        this.scenarios = Array();
+        // {"Label1":5, "Label4":88}
+        this.labels = Array();
+        this.read = Array();
+        // 不需要返回的跳转
+        this.goto = function(label, setRead) {
+            setRead = setRead || false;
+            if (Engine.Proc.labels[label] == undefined)
+                throw new SyntaxError("The label cannot found");
+            let dest = Engine.Proc.labels[label];
+            // 将跳转范围内的文本标记为已读
+            if (setRead && (dest > Engine.Proc.pc))
+                for (let i = Engine.Proc.pc; i < dest; i++)
+                    Engine.Proc.read[i] = true;
+            Engine.Proc.pc = dest;
+        };
 
+        /* 
+        可能不需要这个功能
+        -------------------
+        // 调用栈,call之后需要返回
+        this.callStack = new Array();
+        this.call = function(label) {
+
+        };
+        */
+
+        this.pc = 0;    // 执行到哪一步
+        this.next = function() {
+            if (Engine.Proc.scenarios[Engine.Proc.pc] == undefined)
+                return true;    
+            if (Engine.Control.recordRead)
+                Engine.Proc.read[Engine.Proc.pc] = true;
+            if (Engine.Proc.scenarios[Engine.Proc.pc].func != undefined)    // 表示这是函数不是label
+                // 调用对应function
+                Engine.Proc.scenarios[Engine.Proc.pc].func();
+            else{
+                Engine.Proc.pc ++;
+                return Engine.Proc.next();
+            }
+            Engine.Proc.pc ++;
+            return false;
+        };
+    };
+
+    // 所有渲染相关
     this.Draw = new function() {
         this.MessageLayers = new Array();
         this.PictureLayers = new Array();
@@ -150,11 +167,18 @@ window.Engine = new function(){
             });
 
 
-            this.show = function(animation){
+            // 这个time不仅设置动画的时间,也告诉Control有延时任务
+            this.show = function(animation, time, interrupt, callable){
+                time = time || 0;
+                interrupt = interrupt || true;
 
+                // amination(time);
+
+                if (!interrupt)
+                    Engine.Control.wait(time);
             };
 
-            this.disappear = function(animation) {
+            this.disappear = function(animation, time, interrupt, callable) {
 
             };
 
@@ -228,23 +252,23 @@ window.Engine = new function(){
             };
 
             // 每个图像一层canvas
-            this._src = "";
+            this.src = "";
 
-            this._canvas = document.createElement("canvas");
+            this.canvas = document.createElement("canvas");
             document.body.appendChild(this.canvas);
 
-            this._visible = false;
-            this._alpha = 1;
+            this.visible = false;
+            this.alpha = 1;
 
             // 对图像进行矩形裁剪
-            this._clip = {left:0, top:0, width:0, height:0, enable:false};
-            Object.seal(this._clip);
+            this.clip = {left:0, top:0, width:0, height:0, enable:false};
+            Object.seal(this.clip);
 
             // 缩放, 大于1是放大;小于1是缩小
-            this._scale = 1;
+            this.scale = 1;
             // 上下翻转
-            this._reversVertical = false;
-            this._reversHorizontal = false;
+            this.reversVertical = false;
+            this.reversHorizontal = false;
             // 在画布层的遮罩顺序, zIndex值较大者会覆盖住较小者
             /*
              * 此处的zIndex和MessageLayer中的zIndex互不相干!
@@ -253,23 +277,87 @@ window.Engine = new function(){
              * *** 不建议将两个PictureLayer设置一样的zIndex!(遮罩顺序无法预料) ***
              * MessageLayer永远在PictureLayer之上!
              */
-            this._zIndex = 1000;
-            this._top = this._left = this._bottom = this._right = 0;
+            this.zIndex = 1000;
+            this.top = this.left = this.bottom = this.right = 0;
             // 画布永远是分辨率的长宽,充满整个屏幕, top,left这些属性设置的是图片填充的位置
 
             // 清除画布上的图片和设定的位置,src等参数.
             this.clear = function() {
 
             };
-            this.show = function(animation) {
+            this.show = function(animation, time, interrupt) {
 
             };
-            this.disappear = function(animation) {
+            this.disappear = function(animation, time, interrupt) {
 
             };
         };
     }
 
-    this.Control = new function()｛
+    this.Control = new function() {
+        this.rClickEnabled = true;
+        this.lClickEnabled = true;
+        this.mScrollEnabled = true;
+        this.keyEnterEnabled = true;
+        this.keyCtrlEnabled = true;
+
+        this.onRClick = function() {};
+
+        // 自动阅读模式开启
+        this.auto = false;
+        // 快进模式开启
+        this.skip = false;
+        // 未读部分也快进
+        this.skipNoRead = false;
+
+        // 设置接下来的过程是否记录"已读"
+        this.recordRead = true;
+
+        this.save = function() {
+
+        };
+        this.load = function() {
+
+        };
+
+        this.waitQueue = 0;
+        // 需要在wait过程中暂存的操作设置项名
+        const NEED_TEMP_ENABLEDS = ["rClickEnabled", "lClickEnabled", "mScrollEnabled", "keyEnterEnabled", "keyCtrlEnabled"];
+        this.tempEnableds = {};  // 暂存lClickEnable,keyEnterEnabled等操作设置
+        this.wait = function(time, callable) {
+            if (Engine.Control.waitQueue == 0) {    // 暂存操作设置
+                for (let x in NEED_TEMP_ENABLEDS) {
+                    Engine.Control.tempEnableds[NEED_TEMP_ENABLEDS[x]] = Engine.Control[NEED_TEMP_ENABLEDS[x]];
+                }
+            }
+            for (let x in NEED_TEMP_ENABLEDS) {
+                // 禁止用户操作
+                Engine.Control[NEED_TEMP_ENABLEDS[x]] = false;
+            }
+            Engine.Control.waitQueue ++;
+            setTimeout(function(){
+                if (callable != undefined) callable();
+                Engine.Control.waitQueue --;
+                if (Engine.Control.waitQueue <= 0) {
+                    for (let x in NEED_TEMP_ENABLEDS)
+                    Engine.Control[NEED_TEMP_ENABLEDS[x]] = Engine.Control.tempEnableds[NEED_TEMP_ENABLEDS[x]];
+                    Engine.Control.waitQueue = 0;
+                }
+            }, time);
+        };
+
     };
 }
+
+function ud(){
+        // 每16ms更新画布
+        setInterval(function(){
+            var arr = Engine.Draw.PictureLayers.concat();
+            arr.sort(function(a, b){
+                return a.zIndex - b.zIndex;
+            });
+            for(let i in arr) {
+                i.update();
+            }
+        }, 16);
+    }
